6f3b8551ac0d65a34df56e841f99de804eddbbf6
Garrison <79775636+segarrison@users.noreply.github.com>
2021-10-01 03:02:12 -0500

    Added save functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // save reference to important DOM elements
 var timeDisplay = $("#currentDay");
+var saveBtnEl = $("#button-addon2");
 
 // handle displaying the time
 function displayTime() {
@@ -7,6 +8,21 @@ function displayTime() {
   timeDisplay.text(rightNow);
   console.log(timeDisplay.text);
 }
+
+// $("textarea").each(function (index) {
+//   console.log(index + ":" + $(this).text());
+// });
+$(saveBtnEl).on("click", function (event) {
+  for (i = 9; i < 18; i++) {
+    timeEntry = "#" + [i];
+    console.log(timeEntry);
+    hourEvent = $(timeEntry).val();
+    console.log(hourEvent);
+    localStorage.setItem(timeEntry, hourEvent);
+  }
+});
+
+// saveBtnEl.click(saveEntry());
 
 //gets the time every second
 setInterval(displayTime, 1000);
