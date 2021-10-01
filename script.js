db2cac0fe156cefce017ba8d2fb6c37265ed3e7d
Garrison <79775636+segarrison@users.noreply.github.com>
2021-10-01 03:29:01 -0500

    Added color indicators for past, present, and future times

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ function displayTime() {
   console.log(timeDisplay.text);
 }
 
-// $("textarea").each(function (index) {
-//   console.log(index + ":" + $(this).text());
-// });
+//save entries on button click
 $(saveBtnEl).on("click", function (event) {
   for (i = 9; i < 18; i++) {
     timeEntry = "#" + [i];
@@ -21,8 +19,25 @@ $(saveBtnEl).on("click", function (event) {
     localStorage.setItem(timeEntry, hourEvent);
   }
 });
+//
+function colorBlock() {
+  for (i = 9; i < 18; i++) {
+    timeEntry = "#" + [i];
 
-// saveBtnEl.click(saveEntry());
+    if (i < moment().format("H")) {
+      // console.log(moment().add(7, h).format("H"));
+      $(timeEntry).addClass("past");
+      $(timeEntry).removeClass("future present");
+    } else if (i == moment().format("H")) {
+      $(timeEntry).addClass("present");
+      $(timeEntry).removeClass("future past");
+    } else {
+      $(timeEntry).addClass("future");
+      $(timeEntry).removeClass("present past");
+    }
+  }
+}
 
+$(document).ready(colorBlock);
 //gets the time every second
 setInterval(displayTime, 1000);
