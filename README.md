83a0455e021e2ca77f66cb35c6f8d889a2212efe
Garrison <79775636+segarrison@users.noreply.github.com>
2021-10-01 04:05:26 -0500

    Functionality to reload saved items and updated README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # DayPlanner
-Simplified day planner
+
+## Description
+
+The purpose of this project was to create a simplified day planner for a work day. The user can input and save events, and can easily see past, current, and future events based on intuitive color coding. For even greater utility, the current day and time is set at the top of the page.
+
+## Screenshot of Website
+
+Here's a screen shot showing the color coding based on the time
+![screenshot showing day planner with past hours grayed out, current hour in red, and future hours in green](./workingapp.png)
+
+## Deployed Website
+
+Please visit [github pages for repo](https://segarrison.github.io/DayPlanner/) to see the deployed website.
+
+##Technology
+
+This site was built using:
+*Bootstrap
+*Jquery
+\*Moment.js
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // save reference to important DOM elements
 var timeDisplay = $("#currentDay");
-var saveBtnEl = $("#button-addon2");
+var saveBtnEl = $(".btn");
 
 // handle displaying the time
 function displayTime() {
@@ -19,13 +19,21 @@ $(saveBtnEl).on("click", function (event) {
     localStorage.setItem(timeEntry, hourEvent);
   }
 });
-//
+//iterates through the keys in the local storage and returns their values
+function loadSaved() {
+  for (i = 9; i < 18; i++) {
+    timeEntry = "#" + [i];
+    console.log(timeEntry);
+    console.log(localStorage.getItem(timeEntry));
+    $(timeEntry).val(localStorage.getItem(timeEntry));
+  }
+}
+//for loop with if/else to set colors on past, present, and future time blocks
 function colorBlock() {
   for (i = 9; i < 18; i++) {
     timeEntry = "#" + [i];
 
     if (i < moment().format("H")) {
-      // console.log(moment().add(7, h).format("H"));
       $(timeEntry).addClass("past");
       $(timeEntry).removeClass("future present");
     } else if (i == moment().format("H")) {
@@ -37,7 +45,9 @@ function colorBlock() {
     }
   }
 }
-
+//checks what color time blocks should be on page load
 $(document).ready(colorBlock);
+//checks for saved events on page load
+$(document).ready(loadSaved);
 //gets the time every second
 setInterval(displayTime, 1000);
